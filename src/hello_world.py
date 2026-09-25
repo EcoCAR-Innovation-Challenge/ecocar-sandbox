5e227376d6d768d1b66ecb39fcad192038ee3170
James Keller <RZ8706@NAM.corp.gm.com>
2026-09-25 10:54:05 -0400

    it does math now

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,6 +1,11 @@
 import time
 
 while true:
-    print("hello world")
-    time.sleep(5)
+    print("i am divider")
+    time.sleep(2)
+    num_1 = input(r"enter the numerator: ")
+    num_2 = input(r"enter the denominator: ")
+    answer = num_1/num_2
+    print(f"the answer is {answer}")
+
     
